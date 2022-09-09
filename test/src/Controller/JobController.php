172643178edb57fe56de91bd1519e5fd31715fa9
langeGenial <armel44bambock@gmail.com>
2022-09-09 15:15:42 +0100

    upd ctrl

--- test/src/Controller/JobController.php
+++ test/src/Controller/JobController.php
@@ -3,6 +3,7 @@ namespace App\Controller;
 
 use App\Entity\Jobs;
 use App\Repository\JobsRepository;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -14,25 +15,41 @@ class JobController extends ApiController
     /**
     * @Route("/jobs",methods="GET")
     */
-    public function index(JobsRepository $jobsRepository)
+    public function index(JobsRepository $companiesRepository)
     {
-        $data = $jobsRepository->transformAll();
+        $data = $companiesRepository->transformAll();
         return $this->respond($data);
     }
 
     /**
-    * @Route("/company/{company}/{date}",methods="GET")
+    * @Route("/job/{company}/{date}/{date2}",methods="GET")
     */
-   /* public function findCompany($company, $date, EntityManagerInterface $em, CompaniesRepository $companiesRepository)
+    public function findCompany($company, $date, $date2, EntityManagerInterface $em, JobsRepository $companiesRepository)
     {
         $data = $companiesRepository->transformAll();
         $result = [];
+        $debut = DateTime::createFromFormat('Y-m-d',$date);
+        $debut = date_format($debut,'Y-m-d');
+        $debut = strtotime($debut);
+        $fin = DateTime::createFromFormat('Y-m-d',$date2);
+        $fin = date_format($fin,'Y-m-d');
+        $fin = strtotime($fin);
+        if($debut > $fin){
+            $a = $debut;
+            $debut = $fin;
+            $fin = $a;
+        }
         foreach($data as $item){
-            $mystring = $item["company"];
-            if(preg_match("/{$company}/i", $mystring)){
+            $s1 = strtotime($item['date_published']);
+            $item['comp'] = $company;
+            if($company == -1 ){
+                if($debut<= $s1 & $fin >= $s1){
+                    array_push($result, $item);
+                }
+            }else if(($item['company_id'] == $company) & ($debut<= $s1 & $fin >= $s1)){
                 array_push($result, $item);
             }
         }
         return $this->respond($result);
-    }*/
+    }
 }
